f5576e101609edc15344fbaeaa718e18fa4390e3
Mayur <99334268+devopsmayur@users.noreply.github.com>
2023-04-16 22:03:12 +0530

    Update ec2.tf

--- development/ec2.tf
+++ development/ec2.tf
@@ -5,7 +5,7 @@ module "ec2_cluster" {
   instance_count         = 1
 
   ami                    = "ami-05064bb33b40c33a2"
-  instance_type          = "t2.large"
+  instance_type          = "t2.small"
   key_name               = "pcarey-one"
   monitoring             = true
   vpc_security_group_ids = [module.vpc.default_security_group_id]
