cf2e5deaa5b6e3af5d5512ad59afcf1fb07d7c40
Mayur <99334268+devopsmayur@users.noreply.github.com>
2026-01-01 22:42:02 +0530

    Update main.tf

--- production/main.tf
+++ production/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
   default_tags {   
     tags = {     
         Environment = var.     
-        Owner       = var.owner   
+        Owner       = va
         Project     = var.project
         Terraform   = var.terraform
     }
